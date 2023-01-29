48cfe71262e339fd2b741bce403b10d869e89a19
Mereba11 <abracadabragio12@gmail.com>
2023-01-29 19:05:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -60,6 +60,37 @@ def api_quotes():
 
     return jsonify(all_posts)
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+
+    return render_template('admin.html')
+
+
+@app.route('/admin/verify-passcode', methods=['POST'])
+def verify_passcode():
+    message = 'Acces denied!'
+    passcode = request.form.get('passcode')
+    if passcode == 'passcode':
+        return redirect('/admin/room/IC12S8AfOfPLmaX8rSso7pL6')
+    else:
+        return render_template('error.html', error=message)
+
+@app.route('/admin/room/IC12S8AfOfPLmaX8rSso7pL6', methods=['POST'])
+def admin_room():
+    _posts = db_posts.find()
+
+    return render_template('admin_room.html', posts=_posts)
+
+
+@app.route('/quotes/delete', methods=['DELETE'])
+def post_delete():
+    _id = request.args.get('_id')
+    try:
+        db_posts.delete_one({"_id": ObjectId(_id)})
+        return jsonify({"status": "success"})
+    except:
+        return jsonify({"status": "error"})
+
 
 
 if __name__ == '__main__':
