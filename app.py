80d5196268f96607ffc5b195a1557752779d6a00
Mereba11 <abracadabragio12@gmail.com>
2023-01-29 12:35:15 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
-from flask import Flask, render_template, request, session, jsonify
+from flask import Flask, render_template, request, session, jsonify, redirect, url_for
 from datetime import datetime
+from bson.objectid import ObjectId
 
 from utils.forbidden_words import FORBIDDEN_WORDS
 
@@ -27,7 +28,17 @@ def home():
             'content': post_content,
             'created_at': datetime.now()
         }
-        db_posts.insert_one(query)
+        error_message = 'It is highly forbidden using nsfw words!'
+        found = False
+        for word in FORBIDDEN_WORDS:
+            if word in post_content or word in nickname:
+                found = True
+                break
+        if found:
+            return render_template('error.html', error=error_message)
+        else:
+            db_posts.insert_one(query)
+        
 
     
     return render_template('index.html')
@@ -51,6 +62,38 @@ def api_quotes():
     return jsonify(all_posts)
 
 
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+
+    return render_template('admin.html')
+
+
+@app.route('/admin/verify-passcode', methods=['POST'])
+def verify_passcode():
+    message = 'Acces denied!'
+    passcode = request.form.get('passcode')
+    if passcode == '':
+        return redirect('/admin/room')
+    else:
+        return render_template('error.html', error=message)
+
+@app.route('/admin/room', methods=['GET', 'POST'])
+def admin_room():
+    _posts = db_posts.find()
+
+    return render_template('admin_room.html', posts=_posts)
+
+
+@app.route('/quotes/delete', methods=['DELETE'])
+def post_delete():
+    _id = request.args.get('_id')
+    try:
+        db_posts.delete_one({"_id": ObjectId(_id)})
+        return jsonify({"status": "success"})
+    except:
+        return jsonify({"status": "error"})
 
 if __name__ == '__main__':
     app.run(debug=True)
