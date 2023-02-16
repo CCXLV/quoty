8f92875c43c4d19066af438991eb36d57189123d
Mereba11 <abracadabragio12@gmail.com>
2023-02-16 13:03:37 +0400

    Update forbidden_words.py

--- utils/forbidden_words.py
+++ utils/forbidden_words.py
@@ -1,12 +1,5 @@
 
 FORBIDDEN_WORDS = [
-    'fuck', 'yle', 'ყლე',
-    'მოვტყან', 'movtyan',
-    'FUCK', 'YLE', 'MOVTYAN',
-    'BOZI', 'bozi', 'muteli',
-    'MUTELI', 'მუტელი', 'ბოზი', 
-    'porn', 'PORN', 'YLEO', 'yleo',
-    'ყლეო', 'motynuli', 'MOTYNULI',
-    'მოტყნული', 'bitch'
+    'fuck',
 ]
 
